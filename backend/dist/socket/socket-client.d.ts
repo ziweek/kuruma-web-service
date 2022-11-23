71ec4537ed1e30c88088c6793c39c8939320021f
alex <alex.jiuk.kim@mgmail.com>
2022-11-24 03:17:01 +0900

    .

--- backend/dist/socket/socket-client.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { Socket } from 'socket.io-client';
-export declare class SocketClient implements OnModuleInit {
-    socketClient: Socket;
-    constructor();
-    onModuleInit(): void;
-    private registerConsumerEvents;
-}
