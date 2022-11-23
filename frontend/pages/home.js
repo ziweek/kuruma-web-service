66e5640ce7020eda512a8176527f2e8ffb8385d5
alex <alex.jiuk.kim@mgmail.com>
2022-11-23 14:07:08 +0900

    add w-full

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -102,7 +102,7 @@ export default function Home() {
             </div>
           ))}
 
-          <div className="fixed bottom-0 z-20">
+          <div className="fixed bottom-0 z-20 w-full">
             <div className={isVisibleForBottomDrawer ? "block" : "hidden"}>
               <div
                 id="drawer-bottom-example"
