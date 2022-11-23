44db4eb93a387152839af539ae2902264b62019a
alex <alex.jiuk.kim@mgmail.com>
2022-11-24 02:50:18 +0900

    change package.json

--- frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/congratulations":["static\u002Fchunks\u002Fpages\u002Fcongratulations.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcongratulations"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
