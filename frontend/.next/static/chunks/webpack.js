2d77bc152f99f0c0c95c51b6698ab0c8e684a7b8
alex <alex.jiuk.kim@mgmail.com>
2022-11-24 21:58:37 +0900

    modify frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d5b805eff8904887"; }
+/******/ 		__webpack_require__.h = function() { return "cb711687b53b3590"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
